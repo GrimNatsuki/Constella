b50ca9d43496e0a63803f059965a4ec13491665b
Grim Natsuki <ej2575.mojang@gmail.com>
2026-08-14 21:48:44 +0800

    Internal and State conditions
    - note occlusion and activation logic now relies on note states.
    - internal conditions modify note states, and the states are used as checkers

--- scripts/constellation.gd
+++ scripts/constellation.gd
@@ -13,11 +13,11 @@ var timing_circle
 var chart_active: bool = false
 var song_playing: bool = false
 
-var song_length: float = 6
+var song_length: float = 20
 var bpm: float = 138 
 var subdivision_per_beat: int = 12
 var beats_per_bar: int = 4
-var countdown = 4
+var countdown = 3
 
 var seconds_per_beat:float = 60.0/bpm
 var seconds_per_subdivision: float = 60.0/bpm/subdivision_per_beat
@@ -49,25 +49,23 @@ func set_note_timing(bar:int, beat:int, sub:int)->float:
 func set_note_position(x:float, y:float)->Vector2:
 	return Vector2(x*grid_size.x, y*grid_size.y)
 
-var note_counter:int = 0
 func create_note(x: float, y:float, size_scale:float, bar:int, beat:int, sub:int)->void:
 	note = scene.instantiate()
 	note.pos_coord = Vector2(x, y)
 	note.position = Vector2(x*grid_size.x, y*grid_size.y)
 	note.radius = grid_size.y*size_scale
 	note.timing = set_note_timing(bar, beat, sub)
-	note.z_index = -note_counter
-	
+
 	if !notes.is_empty():
-		for i in range(notes.size()-1, -1, -1): #this counts n,...3, 2, 1
+		for i in range(notes.size()-1, -1, -1): #this counts n,...3, 2, 1, 0
 			var x_dis:float = abs(notes[i].pos_coord.x - note.pos_coord.x)
 			var y_dis:float = abs(notes[i].pos_coord.y - note.pos_coord.y)
 			if (x_dis < 8.0) and (y_dis <8.0):
-				note.blocking_note_id = i
-				break #break if already assigned an id
-	note_counter += 1
-	print(note.blocking_note_id)
+				note.blocked_by_id = i
+				notes[i].blocks_ids.append(notes.size())
 	notes.append(note)
+	note.z_index = -(notes.size())
+	add_child(note)
 
 func _ready() -> void:
 	init_grid()
@@ -77,45 +75,50 @@ func _ready() -> void:
 	scene = preload("res://scenes/note.tscn")
 	
 	create_note(center.x		, center.y			, note_scale, 0, 0, 0)
-	create_note(center.x - 20.0	, center.y			, note_scale, 0, 1, 0)
-	create_note(center.x		, center.y - 4.0	, note_scale, 0, 2, 0)
-	create_note(center.x + 20.0 , center.y			, note_scale, 0, 3, 0)
-	create_note(center.x + 20.0	, center.y - 8.0	, note_scale, 0, 3, 6)
+	create_note(center.x+8.0 	, center.y-8.0		, note_scale, 0, 0, 0)
 	
+	create_note(center.x 		, center.y-4.0		, note_scale, 0, 1, 0)
+	create_note(center.x+8.0 	, center.y-4.0		, note_scale, 0, 1, 0)
+	
+	create_note(center.x 		, center.y-8.0		, note_scale, 0, 2, 0)
+	create_note(center.x+8.0 	, center.y			, note_scale, 0, 2, 0)
+
 	print("chart start...")
 	print("song starting soon...")
 	
 var song_time: float
 var next_note_id: int = 0
-var note_earliest_active_index: int = 0
 
-var touch_notes_id: Array[int]
-var blocked_notes_id: Array[int]
+var notes_id_pool: Array[int]
+
+var spawned_notes_id: Array[int]
+
+var current_note
+var current_note_id:int
 
 func _process(delta: float) -> void:
 	if chart_active:
-		
-		#for i in range(touch_notes_id.size() -1, -1, -1):
-		for i in range(touch_notes_id.size() -1, -1, -1):
-			if notes[touch_notes_id[i]].touched:
-				remove_child(notes[touch_notes_id[i]])
-				notes[touch_notes_id[i]].active = false
-				touch_notes_id.erase(touch_notes_id[i])
-
-							
 		if song_playing:
 			song_time = song_length - timer.time_left
 			while ((song_time) >= (current_subdivision+1)*seconds_per_subdivision):
 				while next_note_id < notes.size() and notes[next_note_id].timing <= song_time:
-					notes[next_note_id].active = true
-					add_child(notes[next_note_id])
-					touch_notes_id.append(next_note_id)
+					notes[next_note_id].spawn()
+					spawned_notes_id.append(next_note_id)
 					next_note_id += 1
 				current_subdivision += 1
 				if (current_subdivision % subdivision_per_beat == 0):
 					current_beat += 1
 					if (current_beat % beats_per_bar == 0):
 						current_bar +=1
+						
+			for i in range(spawned_notes_id.size() -1, -1, -1):
+				current_note_id = spawned_notes_id[i] #current note id
+				current_note = notes[current_note_id] #this is the note scene instance
+				if current_note.state == current_note.State.HIT:
+					current_note.hit()
+					for j in current_note.blocks_ids.size():
+						notes[current_note.blocks_ids[j]].blocked_by_id = -1
+					
 		else:
 			while(timer.time_left <= countdown):
 				if countdown != 1:
@@ -123,7 +126,17 @@ func _process(delta: float) -> void:
 				countdown -= 1
 				if countdown == 0:
 					print("song start")
+					
 
+				#current_note.active = false
+				
+	#for i in notes.size():
+		#if !notes[i].touched:
+			#for j in notes.size():
+				#if i == notes[j].blocked_by_id:
+					#notes[j].blocked_by_id = -1
+	
+		
 func _on_timer_timeout() -> void:
 	if !song_playing:
 		song_playing = true
--- scripts/note.gd
+++ scripts/note.gd
@@ -1,37 +1,84 @@
 extends Node2D
 
+enum State{
+	INVISIBLE_INACTIVE, #invisible, no touch inputs
+	VISIBLE_INACTIVE, #visible, not in the timing window yet (preview_dur), blocked by other notes, no touch
+	VISIBLE_ACTIVE, #visible, can be touched, no blocking notes
+	HIT #already hit, might still be visible e.g. hit fx, no touch inputs, can't block other notes
+}
+
 @onready var collision_shape_2d = $Area2D/CollisionShape2D
 @onready var timer = $Timer
 
-var touched: bool = false
-var active: bool = false
 var time: float
+
 var note_preview_dur:float = 1.0
 
-var radius: float = 1.0
 var timing: float = 0.0
 var sub: int = 0
 var pos_coord:Vector2
 var note_type = NoteData.NoteType.TAP
 
+var radius: float = 1.0
+
 var white = Color(1.0, 1.0, 1.0, 1.0)
 var black = Color(0.0, 0.0, 0.0, 1.0)
-
 var color:Color
 
-var blocking_note_id:int = -1
+var state 
+
+var touched: bool = false
+var input_active: bool = false
+var blocked_by_id:int = -1
+var blocks_ids:Array[int]
+
+func spawn() -> void:
+	show()
+	activate()
+
+func activate()->void:
+	if blocked_by_id < 0:
+		input_active = true
+
+func hit()->void:
+	hide()
+	print("hit")
 
 func _ready() -> void:
+	hide()
 	timer.start(note_preview_dur)
 	collision_shape_2d.shape.radius = radius
-	
+var one_shot_bool = true
 
 func _process(delta: float) -> void:
 	time = note_preview_dur - timer.time_left
-	if blocking_note_id < 0:
-		color = white
+	
+	if !input_active and !visible:
+		state = State.INVISIBLE_INACTIVE
+	elif !input_active and visible:
+		if touched:
+			state = State.HIT
+		else:
+			state = State.VISIBLE_INACTIVE
+	elif input_active and visible:
+		state = State.VISIBLE_ACTIVE
+	
+	if blocked_by_id<0 and !touched:
+		input_active = true
 	else:
-		color = black
+		input_active = false
+		
+	match state:
+		State.INVISIBLE_INACTIVE:
+			pass
+		State.VISIBLE_INACTIVE:
+			color = black
+		State.VISIBLE_ACTIVE:
+			color = white
+		State.HIT:
+			pass
+			#print("note is hit")
+	
 	queue_redraw()
 
 func _on_draw() -> void:
@@ -39,8 +86,11 @@ func _on_draw() -> void:
 	pass
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventScreenTouch:
-		touched = event.pressed
+	if (event is InputEventScreenTouch):
+		if event.pressed and (state == State.VISIBLE_ACTIVE):
+			touched = event.pressed
+			#hide()
+			#set_process(false)
 
 func _on_tree_exited() -> void:
 	pass
